d768c22c00520e87ab7828cd619c0e68aa73aaa4
metud <kvapilmatej@email.cz>
2023-11-03 13:24:33 +0100

    v1.0.0

---
+++ .docker/build/database/create.sql
@@ -0,0 +1,22 @@
+CREATE TABLE "app_products" (
+    id SERIAL PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    price numeric(10,2) NOT NULL,
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE "user" (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    state INT NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL,
+    apikey VARCHAR(255) NOT NULL,
+    last_logged_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL,
+    created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+    updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL
+);
---
+++ db/Fixtures/UserFixture.php
@@ -0,0 +1,88 @@
+<?php declare(strict_types = 1);
+
+namespace Database\Fixtures;
+
+use App\Domain\User\User;
+use App\Model\Fixtures\ReflectionLoader;
+use App\Model\Security\Passwords;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixture extends AbstractFixture
+{
+
+	/** @var ObjectManager */
+	private $manager;
+
+	public function getOrder(): int
+	{
+		return 1;
+	}
+
+	public function load(ObjectManager $manager): void
+	{
+		$this->manager = $manager;
+
+		foreach ($this->getStaticUsers() as $user) {
+			$this->saveUser($user);
+		}
+
+		foreach ($this->getRandomUsers() as $user) {
+			$this->manager->persist($user);
+		}
+
+		$this->manager->flush();
+	}
+
+	/**
+	 * @return mixed[]
+	 */
+	protected function getStaticUsers(): iterable
+	{
+		yield ['email' => 'admin@admin.cz', 'name' => 'Apitte', 'surname' => 'Admin', 'username' => 'apitte', 'role' => User::ROLE_ADMIN, 'apikey' => 'admin'];
+	}
+
+	/**
+	 * @return User[]
+	 */
+	protected function getRandomUsers(): iterable
+	{
+		$loader = new ReflectionLoader();
+		$objectSet = $loader->loadData([
+			User::class => [
+				'user{1..1}' => [
+					'__construct' => [
+						'<firstName()>',
+						'<lastName()>',
+						'<email()>',
+						'<username()>',
+						'<password()>',
+					],
+					'id' => '<(intval(strval($current)))>',
+					'apiKey' => '<sha256()>',
+				],
+			],
+		]);
+
+		return $objectSet->getObjects();
+	}
+
+	/**
+	 * @param mixed[] $user
+	 */
+	protected function saveUser(array $user): void
+	{
+		$entity = new User(
+			$user['name'],
+			$user['surname'],
+			$user['email'],
+			$user['username'],
+			Passwords::create()->hash('admin')
+		);
+		$entity->activate();
+		$entity->setRole($user['role']);
+		$entity->setApikey($user['apikey']);
+
+		$this->manager->persist($entity);
+	}
+
+}
